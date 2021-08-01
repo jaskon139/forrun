599a65d9003f420b0f00c0818cd588d78cc48b42
jaskon139 <jaskon139@gmail.com>
2021-08-01 16:16:02 +0800

    45

--- configrun.sh
+++ configrun.sh
@@ -8,6 +8,8 @@ git clone https://github.com/simo8102/chinaunicom-AutoSignMachine
 
 cd chinaunicom-AutoSignMachine
 
+openssl.exe enc -d -aes-128-cbc -in 321.txt  -k $key  -out 456.txt
+
 npm install
 
 node index.js unicom --user 11111 --password 1111 --appid 1555555
