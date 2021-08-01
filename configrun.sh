9f373a108242b16a93b0c56d20e9e7476a62d217
jaskon139 <jaskon139@gmail.com>
2021-08-01 16:25:46 +0800

    343

--- configrun.sh
+++ configrun.sh
@@ -12,6 +12,8 @@ openssl.exe enc -d -aes-128-cbc -in 321.txt  -k $key  -out 456.txt
 
 value=`cat 456.txt` 
 
+echo $value
+
 npm install
 
 node index.js unicom --user 18626875205 --password $value --appid 2a834d71bd261a5e0aaf6ab3342de851178991d5c86e325e26c77ae58fa9ef642169e9ffd6c1efa2b2ffd4f3eb99ffb3478961ae1ba10c166cc49c6d8d4b1372cca555da7ab9b6d375fc9be63fa19a57
