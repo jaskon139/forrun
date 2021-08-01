52f9793075e40bdaa39ce1b2efca820df9ee19e2
jaskon139 <jaskon139@gmail.com>
2021-08-01 11:02:49 +0800

    2323

--- configrun.sh
+++ configrun.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-apt install nodejs -y
+apt install nodejs npm -y
 
 git clone https://github.com/simo8102/chinaunicom-AutoSignMachine
 
 cd chinaunicom-AutoSignMachine
+
+
 
 node index.js unicom --user 11111 --password 1111 --appid 1555555
