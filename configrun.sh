4938f08117ef39935fcef09b4398282cdca0049f
jaskon139 <jaskon139@gmail.com>
2021-08-13 12:43:31 +0800

    ff

--- configrun.sh
+++ configrun.sh
@@ -30,4 +30,4 @@ node index.js unicom --user 18626875205 --password $value --appid 2a834d71bd261a
 
 node index.js unicom --user 13110676158 --password $value2 --appid 2a834d71bd261a5e0aaf6ab3342de851178991d5c86e325e26c77ae58fa9ef642169e9ffd6c1efa2b2ffd4f3eb99ffb3478961ae1ba10c166cc49c6d8d4b1372cca555da7ab9b6d375fc9be63fa19a57
 
-#node index.js unicom --user 13226453161 --password $value2 --appid 2a834d71bd261a5e0aaf6ab3342de851178991d5c86e325e26c77ae58fa9ef642169e9ffd6c1efa2b2ffd4f3eb99ffb3478961ae1ba10c166cc49c6d8d4b1372cca555da7ab9b6d375fc9be63fa19a57
+node index.js unicom --user 13226453161 --password $value2 --appid 2a834d71bd261a5e0aaf6ab3342de851178991d5c86e325e26c77ae58fa9ef642169e9ffd6c1efa2b2ffd4f3eb99ffb3478961ae1ba10c166cc49c6d8d4b1372cca555da7ab9b6d375fc9be63fa19a57
